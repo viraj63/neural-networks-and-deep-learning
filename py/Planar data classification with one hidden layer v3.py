8f50702718d3ac60efd6e0ea72467de5815cca8c
viraj63 <63658258+viraj63@users.noreply.github.com>
2023-01-19 19:51:12 -0500

    Update Planar data classification with one hidden layer v3.py
    At line #737 cost =cost = compute_cost(A2, Y)  ,
    The previous code was cost =cost = compute_cost(A2, Y,parameters)   #showed error

--- py/Planar data classification with one hidden layer v3.py
+++ py/Planar data classification with one hidden layer v3.py
@@ -733,8 +733,8 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
         # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
         A2, cache = forward_propagation(X,parameters)
         
-        # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
-        cost = compute_cost(A2, Y, parameters)
+        # # Cost function. Inputs: "A2, Y". Outputs: "cost".
+        cost = compute_cost(A2, Y)  
  
         # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
         grads = backward_propagation(parameters, cache, X, Y)
